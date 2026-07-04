57ad038bbd949efc9fd48db3484695088099b615
Ethan Lipnik <hi@ethanlipnik.com>
2026-07-03 22:34:12 -0700

    Cut over repository to Swift Metal package

--- build-steamrt.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker run -it --init --rm -u $UID -w /pyro -v $(pwd):/pyro registry.gitlab.steamos.cloud/steamrt/sniper/sdk ./build-steamrt-helper.sh
-
--- slangmosh.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-slangmosh --output shaders/slangmosh.hpp shaders/slangmosh.json --namespace PyroWave -O --strip
